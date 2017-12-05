dcd06119bb9419396cbbce3d7579b985fc245f3a
ANDERS L KOLSTAD <anders.kolstad@ntnu.no>
2017-12-05 15:39:08 +0100

    The checking for matching location between the new dataset and Natron is treated in a seperate function. Close to complete. Missing UUIDs

--- R/locality_check.R
+++ R/locality_check.R
@@ -32,7 +32,9 @@ con<-dbConnect(pg_drv,
 
 
 
-# dummy data:
+# -----------------------------------------------#
+# Get dummy data   ---------------------------####
+# -----------------------------------------------#
 library(readr)
 flatt_data <- read_csv("flat_data_dummy_std_long.csv")
 
@@ -58,12 +60,29 @@ Natron_location <- dbGetQuery(con,
                              ;")
 
 
+# -----------------------------------------------#
+# Make locations lable         ---------------####
+# -----------------------------------------------#
+loc_db_terms <- tableinfo$column_name[tableinfo$table_name=="Locations"]
+loc_terms <- names(flatt_data)[names(flatt_data) %in% loc_db_terms]
+loc_data_temp <- flatt_data[loc_terms]
+# remove repeated locations
+loc_data_temp2 <- loc_data_temp[!duplicated(loc_data_temp$decimalLongitude),]
+# create empty dataframe with all location table terms
+loc_data_temp3 <- data.frame(matrix(ncol = length(loc_db_terms), nrow = 0),stringsAsFactors=FALSE)
+colnames(loc_data_temp3) <- loc_db_terms
+# rowbind location data to the empty data.frame
+# in order to create location table identical to the one in NaTron
+loc_data_temp4 <- bind_rows(loc_data_temp3, loc_data_temp2)
+
+
+
 #-----------------------------------------------###
 # Function starts                   -----------####
 #-----------------------------------------------###
 
-location_check <- function(flatt_data,conn) {
-  require(tidyverse)
+#location_check <- function(flatt_data,conn) {
+#  require(tidyverse)
 
 
 # Creating an empty dataframe to go into the for-loop below
@@ -82,11 +101,15 @@ dupl_locations <- dbGetQuery(con,
 
 ord <- colnames(dupl_locations)
 dupl_locations$newLocality <- ""
-dupl_locations2 <- dupl_locations[,c("newLocality", ord)]
+dupl_locations$newLat      <- ""
+dupl_locations$newLong     <- ""
+new <- c("newLocality", "newLat", "newLong")
+
+dupl_locations2 <- dupl_locations[,c(new, ord)]
 dupl_locations3 <- dupl_locations2[-c(1:nrow(dupl_locations2)),]
 rm(dupl_locations, dupl_locations2)
 
-# A for-loop to produce SQL query snetences for each locality
+# A for-loop to produce SQL query sentences for each locality, filtering by a geographic radius
 temp_sql <- ""
 for(HEY in 1:nrow(loc_data_temp2)){
   temp_sql[HEY] <-  paste("SELECT",
@@ -102,31 +125,71 @@ for(HEY in 1:nrow(loc_data_temp2)){
                           "\"localityGeom\",((10000 * 180.0) / pi()) / 6378137.0);",
                           sep = " ")
 }
-temp_sql[1]
+# the radius should be reduced to maybe 100, here it's 10000
 
 
 
 # Combining all positive macthes into one dataframe
 temp <- ""
-locality_check <- ""
+locality_check <- dupl_locations3
 
 for(HEY in 1:nrow(loc_data_temp2)){
-  temp2 <- ""
+  temp <- ""
   temp <- dbGetQuery(con, temp_sql[HEY])
-  if(nrow(temp>0)) temp2 <- temp
-  temp2$newLocality <- rep(loc_data_temp2[HEY, "locality"], times=nrow(temp2))
-  temp2 <- temp2[,c("newLocality", ord)]
-  locality_check <- rbind(dupl_locations3, temp2)
-}
 
-
-
-
-
-
-
-
-
-
+  if(dim(temp)[1] !=0) {
+    temp2 <- temp;
+    temp2$newLocality <- rep(loc_data_temp2[HEY, "locality"], times=nrow(temp2));
+    temp2$newLat <- rep(loc_data_temp2[HEY, "decimalLatitude"], times=nrow(temp2));
+    temp2$newLong <- rep(loc_data_temp2[HEY, "decimalLongitude"], times=nrow(temp2));
+    temp2 <- temp2[,c(new, ord)];
+    locality_check <- rbind(locality_check, temp2);
+    rm(temp2)}
 
 }
+#View(locality_check)
+#return(locality_check)
+cat(
+  length(unique(locality_check$newLocality)), "of your locations have possible matches in NaTron.\n",
+  paste(nrow(loc_data_temp2)-length(unique(locality_check$newLocality)),
+  "of your locations had no existing locations within a XX m radius."))
+
+head(loc_data_temp2 )
+names(loc_data_temp2)
+
+# END FUNCTION
+
+# After manualy checking 'locality_check' table for possible reuse of NaTron locations,
+# you may choose resue some locations, but not all.
+# The next function lets you remove newLocations from the 'locations_check table if you think
+# they should be imported into Natron. The newLocations not removed will not be upserted to NaTron,
+# insted we will get the locationIDs from the altermnative locations and use them in the event table.
+
+
+paste(unique(locality_check$newLocality))
+newLocalitySub <- c(1:50, 55:100)
+# the newLocality argument is a vector of the newLocations that you want to import to NaTron
+# (i.e. those that do not have any pre-existing locations in NaTron that could've been reused)
+# indexed by the numerical order
+# (i.e. the order they appear after entering unique(locality_check$newLocality))
+
+
+function(locality_check, loc_data_temp4,  newLocality){
+  locality_check2 <- subset(locality_check, !(locality_check$newLocality %in% unique(newLocality)[newLocalitySub]))
+  locationTable <- subset(loc_data_temp4, !(loc_data_temp4$locality %in% locality_check2$newLocality))
+}
+
+# NEW FUNCTION
+
+
+
+
+function(temp_location_table){
+locationTable <- subset(loc_data_temp4, !(loc_data_temp4$locality %in% locality_check$newLocality))
+# create UUIDS for these
+
+# for a subset of the others, we get the UUIDs from NaTron
+
+}
+
+#}
