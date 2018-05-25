0287e93fb6aa357588136d2159a75e93ace9649d
samaperrin <sam.perrin@ntnu.no>
2018-05-25 14:53:44 +0200

    Deleted rogue bracket

--- R/natron_connect.R
+++ R/natron_connect.R
@@ -6,7 +6,7 @@ natron_connect <- function(username, database = "natron_sandbox") {
 pg_drv <- "PostgreSQL"
 pg_db <- "natron_sandbox"
 pg_host <- "vm-srv-zootron.vm.ntnu.no"
-password=getPass::getPass("Please enter password"))
+password=getPass::getPass("Please enter password")
 pg_db <- database
 
 con<-dbConnect(pg_drv,
