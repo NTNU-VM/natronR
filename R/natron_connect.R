2f6c38ccdfadb56f6d79b2a95854fcd429533ca7
samaperrin <sam.perrin@ntnu.no>
2018-05-25 14:44:09 +0200

    Edited getPass usage

--- R/natron_connect.R
+++ R/natron_connect.R
@@ -6,13 +6,13 @@ natron_connect <- function(username, database = "natron_sandbox") {
 pg_drv <- "PostgreSQL"
 pg_db <- "natron_sandbox"
 pg_host <- "vm-srv-zootron.vm.ntnu.no"
-
+password=getPass::getPass("Please enter password"))
 pg_db <- database
 
 con<-dbConnect(pg_drv,
                host=pg_host,
                dbname=pg_db,
                user=username,
-               password=getPass::getPass("Please enter password"))
+               password=password)
 return(con)
 }
