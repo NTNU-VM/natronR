402fafe098b764d92d0de251919a2b9a3f561fd8
Anders Lorentzen Kolstad <anderlk@ntnu.no>
2019-08-22 11:05:04 +0200

    added package documentation

---
+++ R/natronbatchupload.R
@@ -0,0 +1,8 @@
+#' natronbatchupload
+#'
+#' a package to facilitate the uploading of datasets to NaTRON.
+#'
+#' @details Please see the vigenette for more details.
+#' @docType package
+#' @name natronbatchupload
+NULL
---
+++ R/zzz.R
@@ -0,0 +1,12 @@
+.onAttach <- function(libname, pkgname){
+  packageStartupMessage(
+    '\n ******************\n
+    Welcome to the package that lets you\n
+    upload datasets to NaTRON.\n
+    ***************************\n'
+  )
+}
+
+
+# future add-on. "Type vignette(package = "natronbatchupload") to learn more.\n"
+
